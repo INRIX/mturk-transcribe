340a9af88c6d4e689177fbb5e8d11c43cf4836ac
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-02-27 15:05:50 -0800

    Complete Asset Update Script

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ Turk.
 
 The git repository is divided into two main branches:
 
-master - The latest production-ready version
-develop - The latest development-ready version
+* **master** - The latest production-ready version
+* **develop** - The latest development-ready version
 
 All new work is branched from and merged back into develop. Only develop is
 ever merged into master - when this happens it is considered a release.
--- bin/mark_show_quality_assets.py
+++ bin/mark_show_quality_assets.py
@@ -9,29 +9,63 @@ import pprint
 import psycopg2
 
 
-def partition_assets(assets):
-    """Partition the given group of assets into those that are show quality and
-    those that are not show quality.
+Asset = collections.namedtuple(
+    'Asset',
+    ['lot_id', 'asset_id', 'asset_category_id',
+     'asset_status_id' 'category_id' 'str_bucket',
+     'str_path', 'dt_create_date'])
 
-    :param assets: The assets for a given lot
-    :type assets: list of tuples
-    :rtype: tuple of tuples - (show quality, not show quality)
+
+def mark_asset_as_show_quality(conn, asset_id):
+    """Mark the given asset as show quality.
+
+    :param conn: A postgresql connection
+    :type conn: psycopg2.Connection
+    :param asset_id: An asset id
+    :type asset_id: str or unicode
     """
-    def as_new_as_pred(new_as_date):
-        return lambda x: x[-1].date() >= new_as_date
-    sorted_assets = sorted(assets, key=lambda x: x[-1].date(), reverse=True)
-    newest_date = sorted_assets[0][-1].date()
-    show_quality = filter(as_new_as_pred(newest_date), assets)
-    not_show_quality = itertools.ifilterfalse(
-        as_new_as_pred(newest_date), assets)
-    return show_quality, list(not_show_quality)
+    cur = conn.cursor()
+    cur.execute(
+        'UPDATE asset SET b_show_quality=TRUE WHERE pk_asset=%s;',
+        (asset_id,))
+
+
+def mark_asset_as_approved(conn, asset_id):
+    """Mark the given asset as approved.
+
+    :param conn: A postgresql connection
+    :type conn: psycopg2.Connection
+    :param asset_id: An asset id
+    :type asset_id: str or unicode
+    """
+    cur = conn.cursor()
+    cur.execute(
+        'UPDATE asset SET pk_asset_status=2 WHERE pk_asset=%s;'
+        (asset_id,))
+
+
+def has_associated_lot_asset_types(conn, asset_id):
+    """Indicates whether or not the lot with the given ID has any associated
+    asset types.
+
+    :param asset_id: An asset id
+    :type asset_id: str or unicode
+    :rtype: bool
+    """
+    cur = conn.cursor()
+    cur.execute(
+        'SELECT COUNT(*) FROM asset_lot_asset_type_xref WHERE pk_asset=%s;',
+        (asset_id,))
+    result = cur.fetchone()
+    return result[0] > 0L
 
 
 if __name__ == '__main__':
     conn = psycopg2.connect("dbname=pim user=pim")
     cur = conn.cursor()
     cur.execute('''
-    SELECT asset.pk_lot, pk_asset, str_bucket, str_path, asset.dt_create_date
+    SELECT asset.pk_lot, pk_asset, pk_asset_category,
+    str_bucket, str_path, asset.dt_create_date
     FROM asset
     LEFT JOIN lot USING (pk_lot)
     WHERE
@@ -43,11 +77,9 @@ if __name__ == '__main__':
     for each in cur:
         lot_to_assets[each[0]].append(each)
     for lot_id, assets in lot_to_assets.iteritems():
-        print "[{}]".format(lot_id)
-        pprint.pprint(assets)
-        show_quality, not_show_quality = partition_assets(assets)
-        print "[SHOW QUALITY]"
-        pprint.pprint(show_quality)
-        print "[NOT SHOW QUALITY]"
-        pprint.pprint(not_show_quality)
+        assets = [Asset(*each) for each in assets]
+        for each in assets:
+            if has_associated_lot_asset_types(conn, each.asset_id):
+                mark_asset_as_show_quality(conn, each.asset_id)
+            mark_asset_as_approved(conn, each.asset_id)
     conn.close()
