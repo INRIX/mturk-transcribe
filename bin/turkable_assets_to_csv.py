512c30785b29434e5b4cca5952f2bfb98f53066b
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-03-03 11:08:13 -0800

    Revise Assets To CSV Script
    * Based on Felix's comments we need to do a bit more filtering to get the
      correct set of images we should be sending to mechanical turk.

--- bin/turkable_assets_to_csv.py
+++ bin/turkable_assets_to_csv.py
@@ -2,6 +2,7 @@
 import sys
 sys.path.append('')
 
+import collections
 import csv
 import datetime
 
@@ -19,6 +20,31 @@ def convert_row_to_csv_row(row):
     return 'http://{}/{}'.format(row[1], row[2]), row[0]
 
 
+def get_most_recent_assets(assets):
+    """Return only the most recent assets from the list of assets.
+
+    :param assets: A list of assets
+    :type assets: list
+    :rtype: list
+    """
+    def get_photo_timestamp(asset):
+        return asset[3]
+
+    if not assets:
+        return []
+
+    sorted_assets = sorted(assets, key=get_photo_timestamp, reversed=True)
+    newest_date = get_photo_timestamp(sorted_assets[0])
+    one_week_before_newest = newest_date - datetime.timedelta(days=8)
+
+    most_recent_assets = []
+    for each in sorted_assets:
+        if get_photo_timestamp(each) >= one_week_before_newest:
+            most_recent_assets.append(each)
+
+    return most_recent_assets
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print "Usage: turkable_assets_to_csv.py [CSVFILE]"
@@ -29,7 +55,7 @@ if __name__ == '__main__':
     conn = psycopg2.connect("dbname=pim user=pim")
     cur = conn.cursor()
     cur.execute('''
-    SELECT assetct.pk_asset, str_bucket, str_path FROM (
+    SELECT assetct.pk_asset, str_bucket, str_path, dt_photo, pk_lot FROM (
        SELECT
        asset.*, COUNT(asset_lot_asset_type_xref.*) AS category_count
        FROM asset LEFT OUTER JOIN asset_lot_asset_type_xref
@@ -43,9 +69,19 @@ if __name__ == '__main__':
     ''', (datetime.datetime.utcnow().year,))
     output_field_names = ['image_url', 'asset_id']
 
+    # Group assets by lot
+    lots_to_assets = collections.defaultdict(list)
+    for each in cur:
+        lots_to_assets[each[4]] = each
+
+    # Find the most recent set of assets
+    all_most_recent_assets = []
+    for _, assets in lots_to_assets.iteritems():
+        all_most_recent_assets += get_most_recent_assets(assets)
+
     with open(sys.argv[1], 'wb+') as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow(output_field_names)
-        for each in cur:
+        for each in all_most_recent_assets:
             csvwriter.writerow(convert_row_to_csv_row(each))
     conn.close()
